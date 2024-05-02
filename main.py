fd63bc3392e60abbb087c22faf2d954d7e0ec407
Jackisapi <getphyk3d@gmail.com>
2024-05-01 22:51:44 -0600

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 
-def formula(p, d, o, c, m):
-    total = (p * m) + (d) + (o / m) + (c/m)
-    total = total / 12
-    return total
+# def formula(p, d, o, c, m, t):
+#     total = (p * m) + (round(d / t[0], 2)) + (round(o / t[1], 2)) + (round(c / t[2], 2))
+#     total = total / 12
+#     return total
 
+def formula(p,d,o,c,m,t):
+    total = (p*m) + (d/t[0]) + (o/t[1]) + (c/t[2])
+    return total
 
 st.header("Health Insurance calculator")
 
@@ -14,16 +17,20 @@ premium = st.number_input(label="The Ammount You Pay Per Month")
 
 st.write("Deductables")
 deduct = st.number_input(label="The Ammount You Pay For Your Insurance To 'Kick In ")
+d_time = st.number_input(label="times you pay per month")
 
 st.write("Out of pockets")
 oop = st.number_input("Expensenses not covered by insurance ")
+o_time = st.number_input(label="times you pay per month ")
 
 st.write("CoPayments")
 copay = st.number_input("Payments For Things Like Meds and ER Visits")
+c_time = st.number_input(" times you pay per month")
 
 st.write("Months")
 months = st.number_input("NOTE payments are assumed to be payed monthly frequensy boxes coming soon")
-
 go = st.button("Calculate")
+times = (d_time, o_time, c_time)
 if go:
-    st.write(str(formula(premium, deduct, oop, copay, months)) + " Per month")
+    st.write(str(formula(premium, deduct, oop, copay, months, times)) + " Per Year (Aprox)")
+    st.write(str(formula(premium, deduct, oop, copay, months, times)/12) + " Per month (Aprox)")
